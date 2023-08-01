89acba0ccf417f3d87267856f0af7391e7a37174
Rexx Alfaro <121982791+x3rD1@users.noreply.github.com>
2023-08-01 11:40:59 -0700

    Update script.js
    Change volume from 10000 to 1000

--- js/script.js
+++ js/script.js
@@ -16,10 +16,10 @@ closemoreMusic = musicList.querySelector("#close");
 function minVol(){
   mainAudio.volume = 0.1;
   vol.oninput = function (){
-    mainAudio.volume = vol.value/10000;
+    mainAudio.volume = vol.value/1000;
   }
   if (vol.value != 0.1){
-    mainAudio.volume = vol.value/10000;
+    mainAudio.volume = vol.value/1000;
   }
 }
 
